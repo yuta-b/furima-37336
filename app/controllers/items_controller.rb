1a9b20a24c975c1d162aba1fe681d385ca4d4fc7
yuta-b <yuta1080279@gmail.com>
2022-05-30 19:29:05 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at:"desc")
@@ -21,13 +22,37 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+    if @item.present?
+      redirect_to root_path
+    end
+
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:product_name, :product_version, :category_id, :condition_id, :delivery_charge_id, :delivery_days_id, :delivery_place_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    #redirect_to root_path unless current_user.id == @item.user.id
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-
-  def new
-
+  def show
+    @user = User.find(params[:id])
   end
 end
