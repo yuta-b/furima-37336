c96e5271b9020eaa931c4ef0f368108c9906cb95
yuta-b <yuta1080279@gmail.com>
2022-05-30 12:54:33 +0900

    商品詳細表示機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create ]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @items = Item.all.order(created_at:"desc")
@@ -21,6 +21,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+
   end
 
   private
